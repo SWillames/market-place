6919666a3af95639640aa52f44c9494f148183ec
TatianaRo <tatianavln2012@gmail.com>
2020-09-02 14:04:34 -0300

    Cadastrarum colaborador

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_30_210215) do
+ActiveRecord::Schema.define(version: 2020_09_01_043859) do
 
   create_table "companies", force: :cascade do |t|
     t.string "name"
@@ -21,6 +21,20 @@ ActiveRecord::Schema.define(version: 2020_08_30_210215) do
     t.string "domain"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end   
+  end
 
+  create_table "company_employees", force: :cascade do |t|
+    t.string "full_name"
+    t.string "name"
+    t.date "birth_date"
+    t.string "position"
+    t.string "departament"
+    t.string "cpf"
+    t.integer "company_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["company_id"], name: "index_company_employees_on_company_id"
+  end
+
+  add_foreign_key "company_employees", "companies"
 end
