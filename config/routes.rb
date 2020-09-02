640c22bdeb4ea35b0273286a5cc0d2457d1053d0
TatianaRo <tatianavln2012@gmail.com>
2020-09-02 14:04:34 -0300

    Cadastrar um colaborador

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   resources :companies, only: [ :index, :show, :new, :create]
+  resources :company_employees, only: [ :show, :new, :create]
 end
